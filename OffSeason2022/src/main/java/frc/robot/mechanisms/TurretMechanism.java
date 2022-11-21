864455be1db3a08f96fce1f29a4afd42ec17832d
MasonHarmon <113945967+MasonHarmon@users.noreply.github.com>
2022-11-21 18:03:38 -0500

    Turret Code
    Added turret code

--- OffSeason2022/src/main/java/frc/robot/mechanisms/TurretMechanism.java
+++ OffSeason2022/src/main/java/frc/robot/mechanisms/TurretMechanism.java
@@ -3,15 +3,31 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.mechanisms;
+import java.beans.Encoder;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import frc.robot.FRC5010.Controller;
+import frc.robot.commands.Turret_Left;
+import frc.robot.subsystems.Turret;
 
 /** Add your docs here. */
 public class TurretMechanism {
+    private CANSparkMax turret_motor;
+    private Turret turret;
+    private RelativeEncoder turretEncoder; 
+
 
     public TurretMechanism(){
-        
+        turret_motor = new CANSparkMax(16, MotorType.kBrushless);
+        turret = new Turret(turret_motor);
+        turretEncoder = turret_motor.getEncoder(); 
+        System.out.println(turretEncoder); 
     }
 
     public void configureButtonBindings(Controller driver, Controller coDriver) {
+        coDriver.createAButton().whileHeld(new Turret_Left(turret, 0.1));
+        coDriver.createBButton().whileHeld(new Turret_Left(turret, -0.1));
     }}
